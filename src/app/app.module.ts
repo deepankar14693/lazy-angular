9ddeeacbc1a9fd79fcd0edaa6ee5a138cdf4339e
deepankar14693 <acstar14693@gmail.com>
2019-02-01 08:36:57 +0530

    project structure for lazy loading auth guards remaining

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { NoPageComponent } from './no-page/no-page.component';
+
+import {routes} from './app.router'
+import { HomeModule } from './home/home.module';
+import { IndexModule } from './index/index.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HomeComponent,
+    NoPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    HomeModule,
+    IndexModule
   ],
   providers: [],
   bootstrap: [AppComponent]
